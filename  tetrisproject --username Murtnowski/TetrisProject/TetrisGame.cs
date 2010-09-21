80ad8cbfcf63af1da8b195d48d6d27aab7ee65d3
DamonChastain@gmail.com <DamonChastain@gmail.com@8bc91762-c973-fcef-00d5-670b93667c80>
2010-09-21 20:29:48 +0000

    Increased dimensions of game screen to 1200x900 & created a background color. Set up the world, camera, and projection matrices. Set up the framework for building a cube on the screen. Created a Cube class which encompasses all that is needed in building a cube. Created a pearl texture and built a multi-colored cube on screen in blue background.
    Showed off changed by adding a rotation and scale change to the update.

---  tetrisproject --username Murtnowski/TetrisProject/TetrisGame.cs
+++  tetrisproject --username Murtnowski/TetrisProject/TetrisGame.cs
@@ -18,12 +18,36 @@ namespace TetrisProject
     /// </summary>
     public class TetrisGame : Microsoft.Xna.Framework.Game
     {
+
+        BasicEffect cubeEffect;
+
+        //world matrix
+        Matrix worldMatrix;
+        Matrix cameraMatrix;
+        Matrix projectionMatrix;
+
+        //background
+        Color background;
+        Color blueBackground = new Color(10, 40, 125, 0);
+
+        //screen
+        internal const int SCREEN_WIDTH = 1200;
+        internal const int SCREEN_HEIGHT = 900;
+
+        //angles used in camera rotations
+        int angle1 = 0;
+        int angle2 = 0;
+        int angle3 = 0;
+        float scale = 1.0f;
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
         public TetrisGame()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = SCREEN_WIDTH; //set screen width to 1200
+            graphics.PreferredBackBufferHeight = SCREEN_HEIGHT; //set screen height to 900
             Content.RootDirectory = "Content";
         }
 
@@ -49,7 +73,7 @@ namespace TetrisProject
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            initializeWorld(); //initializes world matrix, cube effect, camera
         }
 
         /// <summary>
@@ -72,6 +96,12 @@ namespace TetrisProject
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
+            //world rotation
+            angle1 += 1;
+            angle2 += 3;
+            scale += .01f;
+            if (scale > 10)
+                scale = 0;
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -83,11 +113,51 @@ namespace TetrisProject
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            //GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.RenderState.DepthBufferEnable = true;
+            GraphicsDevice.RenderState.AlphaBlendEnable = false;
+            GraphicsDevice.RenderState.AlphaTestEnable = false;
 
-            // TODO: Add your drawing code here
+            graphics.GraphicsDevice.Clear(blueBackground);
+            //world matrix alterations are used with variables to save previous changes
+            worldMatrix = Matrix.CreateRotationY(MathHelper.ToRadians(angle1)) *
+                Matrix.CreateRotationX(MathHelper.ToRadians(angle2)) *
+                Matrix.CreateRotationZ(MathHelper.ToRadians(angle3)) *
+                Matrix.CreateScale(scale);
+            cubeEffect.World = worldMatrix;
 
+            cubeEffect.Begin();
+            foreach (EffectPass pass in cubeEffect.CurrentTechnique.Passes)
+            {
+                pass.Begin();
+                Cube cube = new Cube(new Vector3(0, 0, 0));
+                cube.RenderShape(GraphicsDevice);
+                cube.shapeTexture = Content.Load<Texture2D>("Textures\\pearl");
+                cubeEffect.Texture = cube.shapeTexture;
+                pass.End();
+            }
+            cubeEffect.End();
             base.Draw(gameTime);
+        }
+
+        public void initializeWorld()
+        {
+            //first vector controls x,y,z depth, second moves position laterally, third flips board
+            cameraMatrix = Matrix.CreateLookAt(
+                new Vector3(9, 7, 9), new Vector3(-1, -1, -1), new Vector3(0, 1, 0));
+
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4,
+                (float)Window.ClientBounds.Width / (float)Window.ClientBounds.Height, 1.0f, 100.0f);
+
+            //  float tilt = MathHelper.ToRadians(23.0f); //tilt value to use for x & y
+            //  worldMatrix = Matrix.CreateRotationX(tilt) * Matrix.CreateRotationY(tilt);
+            cubeEffect = new BasicEffect(GraphicsDevice, null);
+            // cubeEffect.World = worldMatrix;
+            cubeEffect.View = cameraMatrix;
+            cubeEffect.Projection = projectionMatrix;
+            cubeEffect.TextureEnabled = true;
+            // cubeEffect.EnableDefaultLighting();
+
         }
     }
 }
